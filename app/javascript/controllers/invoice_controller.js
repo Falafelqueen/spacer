6b2d735b94073116a253a61ba4b8e3fc1ab76df4
Lucia Harcegova <fakeluciamail@gmail.com>
2022-12-09 11:25:14 +0100

    active dahsboard tab, price preview, calculation

--- app/javascript/controllers/invoice_controller.js
+++ app/javascript/controllers/invoice_controller.js
@@ -22,10 +22,14 @@ export default class extends Controller {
     }
     const duration = (end - start)
     const parking_price = Number(parking_spot.dataset.price)
-    if(event.currentTarget.checked){
-      total = total + (parking_price * duration)
-    }else{
-      total = total - (parking_price * duration)
+
+    // checking if the duration is returning NaN, if not calculate price preview->
+    if(!Number.isNaN(duration)){
+      if(event.currentTarget.checked){
+        total = total + (parking_price * duration)
+      }else{
+        total = total - (parking_price * duration)
+      }
     }
     this.priceTarget.innerHTML = total
     return total
